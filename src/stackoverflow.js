643d275195742927b972991337c34f0bcdc2d3d1
Mikael Brassman <mishakun@gmail.com>
2013-07-11 22:34:12 +0200

    fixed stackvets output

--- src/stackoverflow.js
+++ src/stackoverflow.js
@@ -1,36 +1,31 @@
 var Q = require('q');
 var _ = require('lodash');
 var request = require('request');
-var cheerio = require('cheerio');
 var print = require('./print');
 var loadSoData = require('./providers/so');
+var moment = require('moment');
 
 module.exports.requestScores = function(ninjas) {
 	return loadSoData(ninjas);
 };
 
-var getIntRep = function(ninja) {
-	var rep = ninja.so.rep;
-	var repInt = parseInt(rep.replace(/,/g, ''), 10);
-	return repInt;
-};
-
 var filtered = function(ninjas) {
 	return _(ninjas).filter(function(ninja) {
-		return ninja.so.rep;
+		return ninja.so.data;
 	});
 };
 
 module.exports.printScores = function(ninjas, options) {
 	print.ranked(filtered(ninjas), options, function(ninja) {
-		return getIntRep(ninja);		
+		return ninja.so.data.reputation;		
 	});
 };
 
 module.exports.printIds = function(ninjas, options) {
 	print.ranked(filtered(ninjas), options, function(ninja) {
-		return -ninja.so.id;		
+		return -ninja.so.data.creation_date;		
 	}, function(ninja) {
-		return ninja.so.id;
+		var millis = ninja.so.data.creation_date*1000;
+		return moment(millis).format();
 	});
 };
