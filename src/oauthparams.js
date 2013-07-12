af0fdd6a467533fc0a28810abb36af14e063ecf3
Mikael Brassman <mishakun@gmail.com>
2013-07-12 19:19:52 +0200

    Using nconf to load params from environment or file

--- src/oauthparams.js
+++ src/oauthparams.js
@@ -1,12 +1,15 @@
 var Q = require('q');
+var nconf = require('nconf');
 
 module.exports.twitter = function() {
 	return Q.fcall(function() {
+		nconf.env().file({file: 'config.json'});
+		
 		var params = {
-			"consumer_key": process.env.TWITTER_CONSUMER_KEY,
-			"consumer_secret": process.env.TWITTER_CONSUMER_SECRET,
-			"oauth_token": process.env.TWITTER_OAUTH_TOKEN,
-			"oauth_token_secret": process.env.TWITTER_OAUTH_TOKEN_SECRET
+			"consumer_key": nconf.get("TWITTER_CONSUMER_KEY"),
+			"consumer_secret": nconf.get("TWITTER_CONSUMER_SECRET"),
+			"oauth_token": nconf.get("TWITTER_OAUTH_TOKEN"),
+			"oauth_token_secret": nconf.get("TWITTER_OAUTH_TOKEN_SECRET")
 		};
 		return params;
 	});
