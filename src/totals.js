f17b1f8be0d58703de80e35e269c9811fa88feeb
Mikael Brassman <mishakun@gmail.com>
2013-07-12 13:28:22 +0200

    Showing top ninja for totals

--- src/totals.js
+++ src/totals.js
@@ -26,29 +26,73 @@ module.exports = function(ninjas, options) {
 		tweets: 0,
 		rep: 0
 	};
+
+	var githubbers = getGitHubbers(ninjas);
+	var twitterers = getTwitterers(ninjas);
+	var stackers = getStackers(ninjas);
+
+	var topNinja = {
+		repos: githubbers.first(),
+		gists: githubbers.first(),
+		tweets: twitterers.first(),
+		rep: stackers.first()
+	};
 	
-	githubbers = getGitHubbers(ninjas);
 	githubbers.each(function (ninja) {
-		sums.repos += ninja.github.data.public_repos;
-		sums.gists += ninja.github.data.public_gists;
+		var repos = ninja.github.data.public_repos;
+		sums.repos += repos;
+		if(topNinja.repos.github.data.public_repos < repos) {
+			topNinja.repos = ninja;
+		}
+		var gists = ninja.github.data.public_gists;
+		sums.gists += gists;
+		if(topNinja.gists.github.data.public_gists < gists) {
+			topNinja.gists = ninja;
+		}
 	});
 	
-	twitterers = getTwitterers(ninjas);
 	twitterers.each(function (ninja) {
-		sums.tweets += ninja.twitter.data.statuses_count;
+		var tweets = ninja.twitter.data.statuses_count
+		sums.tweets += tweets;
+		if(topNinja.tweets.twitter.data.statuses_count < tweets) {
+			topNinja.tweets = ninja;
+		}
 	});
 	
-	stackers = getStackers(ninjas);
 	stackers.each(function (ninja) {
-		sums.rep += ninja.so.data.reputation;
+		var rep = ninja.so.data.reputation;
+		sums.rep += rep;
+		if(topNinja.rep.so.data.reputation < rep) {
+			topNinja.rep = ninja;
+		}
 	});
 	
-	console.log(githubbers.size(), 'ninjas uses Github')
+	console.log(githubbers.size() + ' ninjas uses Github');
 	console.log('Total no. of public repos:\t', sums.repos, 'repos');
+	console.log(' ', topNinja.repos.name, 
+		'has the most with', 
+		topNinja.repos.github.data.public_repos, 
+		"repos");
 	console.log('Total no. of public gists:\t', sums.gists, 'gists');
-	console.log(twitterers.size(), 'ninjas uses Twitter')
+	console.log(' ', topNinja.gists.name, 
+		'has the most with', 
+		topNinja.repos.github.data.public_gists, 
+		"gists");
+		
+	console.log(twitterers.size() + ' ninjas uses Twitter');
 	console.log('Total no. of tweets:\t\t', sums.tweets, 'tweets');
-	console.log(stackers.size(), 'ninjas uses StackOverflow')
+	console.log(' ', topNinja.tweets.name, 
+		'has tweeted the most with', 
+		topNinja.tweets.twitter.data.statuses_count, 
+		"tweets");
+	
+	
+	console.log(stackers.size() + ' ninjas uses StackOverflow');
 	console.log('Total amount of rep:\t\t', sums.rep, 'points');
+	console.log(' ', topNinja.rep.name, 
+		'is the most reputable with', 
+		topNinja.rep.so.data.reputation, 
+		"points");
+	
 	
 };
