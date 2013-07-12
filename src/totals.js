a5de6e6220da787a44e38bbe6b45721dcf4364e7
Mikael Brassman <mishakun@gmail.com>
2013-07-12 13:16:04 +0200

    Printing out number of ninjas that use each social service

--- src/totals.js
+++ src/totals.js
@@ -27,25 +27,28 @@ module.exports = function(ninjas, options) {
 		rep: 0
 	};
 	
-	getGitHubbers(ninjas).each(function (ninja) {
+	githubbers = getGitHubbers(ninjas);
+	githubbers.each(function (ninja) {
 		sums.repos += ninja.github.data.public_repos;
 		sums.gists += ninja.github.data.public_gists;
 	});
 	
-	getTwitterers(ninjas).each(function (ninja) {
+	twitterers = getTwitterers(ninjas);
+	twitterers.each(function (ninja) {
 		sums.tweets += ninja.twitter.data.statuses_count;
 	});
 	
-	getStackers(ninjas).each(function (ninja) {
+	stackers = getStackers(ninjas);
+	stackers.each(function (ninja) {
 		sums.rep += ninja.so.data.reputation;
 	});
 	
-	console.log('Github')
+	console.log(githubbers.size(), 'ninjas uses Github')
 	console.log('Total no. of public repos:\t', sums.repos, 'repos');
 	console.log('Total no. of public gists:\t', sums.gists, 'gists');
-	console.log('Twitter')
+	console.log(twitterers.size(), 'ninjas uses Twitter')
 	console.log('Total no. of tweets:\t\t', sums.tweets, 'tweets');
-	console.log('StackOverflow')
+	console.log(stackers.size(), 'ninjas uses StackOverflow')
 	console.log('Total amount of rep:\t\t', sums.rep, 'points');
 	
 };
