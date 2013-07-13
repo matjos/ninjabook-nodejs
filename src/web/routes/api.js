6acb2140c711e157a563f4ef3e4002ff2b7cebfd
Mikael Brassman <mishakun@gmail.com>
2013-07-13 12:41:56 +0200

    Simple api entry point

--- src/web/routes/api.js
+++ src/web/routes/api.js
@@ -5,9 +5,20 @@ module.exports = function() {
 	var app = express();
 	
 	app.get('/', function(req, res) {
+		res.send({
+			links: [
+				{
+					rel: 'totals',
+					href: '/totals'
+				}
+			]
+		});
+	});
+	
+	app.get('/totals', function(req, res) {
 		getTotalStats().then(function(stats) {
 			res.send(stats);
-		});
+		});		
 	});
 	
 	return app;
